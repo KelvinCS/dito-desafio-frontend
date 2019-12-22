c165f9ad2c59418ecc69d8f6522890cf54455163
Kelvin CS <kelvinh.job@gmail.com>
2019-12-22 17:31:39 -0300

    [doc] Storie added to TimelineEvent

---
+++ src/components/TimelineEvent/TimelineEvent.stories.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import TimelineEvent from "./index";
+
+export default { title: "TimelineEvent" };
+
+export const Event = () => (
+  <TimelineEvent
+    timestamp="2016-09-22T13:57:32.2311892-03:00"
+    location="BH Shopping"
+    total={270}
+    products={[
+      { name: "Tênis azul", price: 120 },
+      { name: "Calça rosa", price: 150 }
+    ]}
+  />
+);
