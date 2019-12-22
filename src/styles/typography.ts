ba26a0e3c2e7ba8930caaf2ff089548fd9e9a81d
Kelvin CS <kelvinh.job@gmail.com>
2019-12-22 17:34:37 -0300

    [styles] Typography created

---
+++ src/styles/typography.ts
@@ -0,0 +1,15 @@
+import { colors } from "./variables";
+
+export const Label = `
+  font-weight: bold;
+  color: ${colors.dark};
+  font-size: 0.875rem;
+  font-family: Roboto
+`;
+
+export const Text = `
+  font-weight: normal;
+  color: ${colors.dark};
+  font-size: 0.875rem;
+  font-family: Roboto
+`;
