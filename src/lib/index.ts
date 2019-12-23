1d1e49f9ab53aa8d741e1c0aea0020cac7276f60
Kelvin CS <kelvinh.job@gmail.com>
2019-12-23 07:54:06 -0300

    [feature]  Function for sorting events by timestamp created

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,5 @@
+import moment from "moment";
+
 /**
  * Formats a number to the brazilian currency
  * @param value
@@ -5,6 +7,8 @@
  * formatCurrency(14.30)
  * @returns "R$ 14,30"
  */
-export const formatCurrency = (value: number): string => {
-  return `R$ ${value.toFixed(2).replace(".", ",")}`;
-};
+export const formatCurrency = (value: number): string =>
+  `R$ ${value.toFixed(2).replace(".", ",")}`;
+
+export const sortByTimestamp = (elementA, elementB) =>
+  moment(elementA.timestamp).unix() - moment(elementB.timestamp).unix();
