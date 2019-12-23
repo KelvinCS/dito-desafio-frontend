661f618d7f94d8a3ff32e74673d4355898dc33e8
Kelvin CS <kelvinh.job@gmail.com>
2019-12-23 07:47:06 -0300

    [feature] Globalstyles added

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import EventTimeline from "./pages/EventTimeline";
+import { ThemeProvider } from "styled-components";
+import GlobalStyles from "./styles/global";
+import styles from "./styles";
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={styles}>
+      <GlobalStyles />
+      <EventTimeline></EventTimeline>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
